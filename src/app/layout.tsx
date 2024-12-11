d47c813a50ea3c8944c471fe257567d7fbf0e638
PrajwalHs2609 <prajwalhs4india@gmail.com>
2024-12-11 23:49:11 +0530

    added favico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,12 +10,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <link rel="icon" type="image/png" href="/faviconPerfect.png" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
       </head>
       <body>
         <Navbar />
         <main> {children}</main>
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );
