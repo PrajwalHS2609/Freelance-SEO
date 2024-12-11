cd221a76651dd2156899339270abb499d7602ba6
PrajwalHs2609 <prajwalhs4india@gmail.com>
2024-12-11 20:36:11 +0530

    added logo

--- src/components/Footer/FooterBio.tsx
+++ src/components/Footer/FooterBio.tsx
@@ -1,13 +1,16 @@
 import "./Footer.css";
 import { FaFacebook, FaYoutube, FaInstagram, FaLinkedin } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
-
+import Image from "next/image";
+import logo from "./logo/TSS.png"
 export default function FooterBio() {
   return (
     <div className="footer-bio">
       <h3>About Us</h3>
       <div className="footer-bioImg">
-        <a href="https://tryseoservices.com/">logo</a>
+        <a href="https://tryseoservices.com/">
+          <Image src={logo} alt="logo" />
+        </a>
       </div>
       <p>
         FreelanceSEO.co.in has consistently delivered top-notch SEO solutions.
@@ -17,27 +20,27 @@ export default function FooterBio() {
       <div className="footer-bioSocial">
         <div className="footer-bioSocialItem">
           <a href="\">
-            <FaFacebook className="footerSocialIcons"/>
+            <FaFacebook className="footerSocialIcons" />
           </a>
         </div>
         <div className="footer-bioSocialItem">
           <a href="\">
-            <FaYoutube className="footerSocialIcons"/>
+            <FaYoutube className="footerSocialIcons" />
           </a>
         </div>
         <div className="footer-bioSocialItem">
           <a href="\">
-            <FaInstagram className="footerSocialIcons"/>
+            <FaInstagram className="footerSocialIcons" />
           </a>
         </div>
         <div className="footer-bioSocialItem">
           <a href="\">
-            <FaLinkedin className="footerSocialIcons"/>
+            <FaLinkedin className="footerSocialIcons" />
           </a>
         </div>
         <div className="footer-bioSocialItem">
           <a href="\">
-            <FaXTwitter className="footerSocialIcons"/>
+            <FaXTwitter className="footerSocialIcons" />
           </a>
         </div>
       </div>
--- src/components/Navbar/Logo.tsx
+++ src/components/Navbar/Logo.tsx
@@ -1,3 +1,9 @@
+import Image from "next/image";
+import logo from "./logo/TSS.png";
 export default function Logo() {
-  return <div className="logo-container"></div>;
+  return (
+    <div className="logo-container">
+      <Image src={logo} alt="logo" />
+    </div>
+  );
 }
