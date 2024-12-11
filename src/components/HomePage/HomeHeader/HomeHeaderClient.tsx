1fba52a1d724e4662022953fd86afc211aa0443e
PrajwalHs2609 <prajwalhs4india@gmail.com>
2024-12-11 20:05:40 +0530

    eslint changes

--- src/components/HomePage/HomeHeader/HomeHeaderClient.tsx
+++ src/components/HomePage/HomeHeader/HomeHeaderClient.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { useEffect, useState } from "react";
 import Image from "next/image";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
