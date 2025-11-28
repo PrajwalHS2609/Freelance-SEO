8c48f98beb0b2cca2bc3ac8354a675694171a7c8
Prajwal H S <prajwalhs4india@gmail.com>
2025-11-28 12:56:43 +0530

    no middle redirect2

--- src/utils/prefixLinks.ts
+++
@@ -1,8 +0,0 @@
-// export function prefixLinks(html: string, country: string) {
-//   if (!country) return html;
-
-//   return html.replace(
-//     /https:\/\/freelanceseo\.co\.in(\/[^"']*)/g,
-//     `https://freelanceseo.co.in/${country}$1`
-//   );
-// }
